3688afd4eb69bdada8c4bbd468c6a6fe43091ab1
ishaan2_4 <ishaanbansal.2003@gmail.com>
2023-10-22 18:47:33 -0500

    Seven bodies!!!!!!!

--- Kronos.py
+++ Kronos.py
@@ -15,11 +15,24 @@ def ivp_rk4(u_0, T, delta_t):
     u = []
     num_elements = int(T / delta_t) + 1
     for t_k in np.linspace(0,T,num_elements):
+        r = u_0[0]
+        dist = []
+        N = r.shape[0]
+        sol_r = []
+        sol_time = 0
+        for i in range(1,N):
+            dist.append(r[i]-r[0])
+        for i in range(1, N-1):
+            if np.dot(dist[i],dist[i-1]) != 1:
+                break
+            elif i == N-2:
+                sol_r = r[2]
+                sol_time = t_k
         times.append(t_k)
         u.append(u_0)
         u_k = rk4(u_0, delta_t)
         u_0 = u_k
-    return np.array(u), times
+    return np.array(u), times, sol_r, sol_time
 
 G = 6.674e-11
 def f_true(u):
@@ -50,31 +63,41 @@ def ab4(u_0,T,delta_t):
     return np.array(u),times
 
 # T, dt = 300 * 365 * 24 * 3600, 60*60*24*100
-T, dt = 24*60*60*365*1, 24*60*60 
-mSaturn = 568.32e24
-mTitan = 1.345e23
-rTitan = -1.22e9
-mDeathStar = 2.24e23
-# mearth = 5.97e24
-mMimas = 3.75e19
-rMimas = 198e6
+T, dt = 24*60*60*365*10, 60*60 
+mSaturn = 568.32e24 # kg
+mTitan = 1.345e23 # kg
+rTitan = -1.22e9 # m
+mDeathStar = 2.24e23 # m
+# mearth = 5.97e24 # kg
+mMimas = 3.75e19 # kg
+rMimas = 189e6 # m
 mHyperion = 5.58*1080 # kg
 rHyperion = 1.5e9 # m
-m = np.array([mSaturn,mTitan,mDeathStar,mHyperion])
-r0 = np.zeros((4,3))
+mIapetus = 1.806*1021 # kg
+rIapetus = 3.56e9 #m
+mFenrir = 1e13 # kg
+rFenrir = 22.5e9 # m
+m = np.array([mSaturn,mTitan,mDeathStar,mHyperion, mIapetus, mFenrir])
+r0 = np.zeros((np.size(m),3))
 r0[0] = [0.0, 0.0, 0.0]
 r0[1] = [rTitan, 0.0,0.0]
-r0[2] = [1.22e9*2, 0.0, 0.0]
 r0[3] = [rHyperion, 0.0, 0.0]
+r0[4] = [rIapetus, 0.0, 0.0]
+r0[5] = [rFenrir, 0.0, 0.0]
+
+r0[2] = [np.max(r0)*2, 0.0, 0.0]
+
 # print(r0)
-v0 = np.zeros((4,3))
+v0 = np.zeros((np.size(m),3))
 v0[0] = [0.0, 0.0, 0.0]
 v0[1] = [0.0, -np.sqrt(G*m[0]/np.linalg.norm(r0[1])), 0.0]
 v0[2] = [0.0, -np.sqrt(G*m[0]/np.linalg.norm(r0[2])), 0.0]
 v0[3] = [0.0, -np.sqrt(G*m[0]/np.linalg.norm(r0[3])), 0.0]
+v0[4] = [0.0, -np.sqrt(G*m[0]/np.linalg.norm(r0[4])), 0.0]
+v0[5] = [0.0, -np.sqrt(G*m[0]/np.linalg.norm(r0[5])), 0.0]
 
 u0 = np.array([r0, v0])
-u_rk, times = ivp_rk4(u0, T, dt)
+u_rk, times, sol_r, sol_time = ivp_rk4(u0, T, dt)
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
@@ -86,20 +109,32 @@ r1 = []
 r2 = []
 r3 = []
 r4 = []
+r5 = []
+r6 = []
+
 for i in range(ur.shape[0]):
     r1.append(ur[i][0])
     r2.append(ur[i][1])
     r3.append(ur[i][2])
     r4.append(ur[i][3])
+    r5.append(ur[i][4])
+    r6.append(ur[i][5])
 r1 = np.array(r1)
 r2 = np.array(r2)
 r3 = np.array(r3)
 r4 = np.array(r4)
+r5 = np.array(r5)
+r6 = np.array(r6)
 
-ax.scatter(r1[:,0], r1[:,1], r1[:,2], color="yellow", label="Saturn")
-ax.scatter(r2[:,0], r2[:,1], r2[:,2], color="blue", label="Titan")
-ax.scatter(r3[:,0], r3[:,1], r3[:,2], color="black", label="Death Star")
-ax.scatter(r4[:,0], r4[:,1], r4[:,2], color="cyan", label="Hyperion")
+print(sol_r)
+print(sol_time)
+ax.plot(r1[:,0], r1[:,1], r1[:,2], color="yellow", label="Saturn")
+ax.plot(r2[:,0], r2[:,1], r2[:,2], color="blue", label="Titan")
+ax.plot(r3[:,0], r3[:,1], r3[:,2], color="black", label="Death Star")
+ax.plot(r4[:,0], r4[:,1], r4[:,2], color="cyan", label="Hyperion")
+ax.plot(r5[:,0], r5[:,1], r5[:,2], color="pink", label="Lapetus")
+ax.plot(r6[:,0], r6[:,1], r6[:,2], color="orange", label="Fenrir")
+ax.scatter(sol_r, "r^", label="Solution")
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
